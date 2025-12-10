d43d6eedc78faff89404403a96b9b9955e02a200
unknown <cleverposter@gmail.com>
2025-12-10 21:50:33 +0530

    first commit

---
+++ lesson 2.2/lesson2.2.js
@@ -0,0 +1,18 @@
+const titleProject = ("Разработка лендинга");
+console.log(typeof titleProject);
+
+const screensValue = ("шаблонные, с уникальным дизайном, с анимациями");
+console.log(typeof screensValue);
+
+const screenPrice  = 100;
+console.log(typeof screenPrice);
+
+
+const percentage = 33;
+console.log(typeof percentage);
+
+const fullPrice = (344498977n);
+console.log(typeof fullPrice);
+
+const responsive = true;
+console.log(typeof responsive);
